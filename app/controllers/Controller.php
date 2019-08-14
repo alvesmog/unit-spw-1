d90d475f7d024e2a16d1ef99c04a7cb8930669ec
alvesmog <41652807+alvesmog@users.noreply.github.com>
2019-08-14 17:05:08 -0300

    abstração controllers

---
+++ app/controllers/Controller.php
@@ -0,0 +1,33 @@
+<?php
+
+//Abstração das classes controllers
+
+abstract class Controller
+{
+    //Propriedades    
+    //Armazena os dados recebidos dos models
+    protected $data = array();
+    //O nome da view que vai ser renderizada
+    protected $view = "";
+    //O head do HTML
+    protected $head = array('title' => '', 'description' => '');
+
+    //Métodos
+    //Processa os parâmetros
+    abstract function process($params);
+    //Renderiza a view para o usuário
+    public function renderView()
+    {
+        if ($this->view) {
+            extract($this->data);
+            require("views/" . $this->view . ".phtml");
+        }
+    }
+    //Finaliza o script
+    public function redirect($url)
+    {
+        header("Location: /$url");
+        header("Connection: close");
+        exit;
+    }
+}
