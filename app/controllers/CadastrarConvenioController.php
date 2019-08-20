a3a0bd0d6c97ebcb04dc02f88e6c332b6c5ff8d7
alvesmog <41652807+alvesmog@users.noreply.github.com>
2019-08-19 21:49:08 -0300

    cadastrar controller

--- app/controllers/CadastrarConvenioController.php
+++ app/controllers/CadastrarConvenioController.php
@@ -8,6 +8,13 @@ class CadastrarConvenioController extends Controller
         header("Cadastrar convênio");
         // HTML title
         $this->head['title'] = 'Cadastrar convênio';
+
+        if (isset($_POST["submit"])) {
+
+            $CadastrarConvenio = new CadastrarConvenio();
+            $CadastrarConvenio->cadastrar($_POST['cod'], $_POST['nome'], $_POST('banco'), $_POST('sequencial'), $_POST('agencia'), $_POST('conta'), $_POST('digito'));
+        }
+
         // Sets the template
         $this->view = 'cadastrar-convenio';
     }
