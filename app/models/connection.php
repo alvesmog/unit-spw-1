9dccc25a6fd3e7b40a5ff256f973d3c34aa57cae
alvesmog <41652807+alvesmog@users.noreply.github.com>
2019-08-20 23:06:33 -0300

    Connection class

--- app/models/connection.php
+++ app/models/connection.php
@@ -1,24 +1,47 @@
 <?php
-Class Connection {
-private  $server = "mysql:host=localhost;dbname=dbspw1";
-private  $user = "root";
-private  $pass = "";
-private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
-protected $con;
- 
-            public function openConnection()
-             {
-               try
-                 {
-          $this->con = new PDO($this->server, $this->user,$this->pass,$this->options);
-          return $this->con;
-                  }
-               catch (PDOException $e)
-                 {
-                     echo "There is some problem in connection: " . $e->getMessage();
-                 }
-             }
-public function closeConnection() {
-     $this->con = null;
+class Db
+{
+  private static $connection;
+  private static $settings = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    PDO::ATTR_EMULATE_PREPARES => false,
+  );
+  public static function connect($host, $user, $password, $database)
+  {
+    if (!isset(self::$connection)) {
+      self::$connection = @new PDO(
+        "mysql:host=$host;dbname=$database",
+        $user,
+        $password,
+        self::$settings
+      );
+    }
+  }
+  public static function queryOne($query, $params = array())
+  {
+    $result = self::$connection->prepare($query);
+    $result->execute($params);
+    return $result->fetch();
+  }
+  public static function queryAll($query, $params = array())
+  {
+    $result = self::$connection->prepare($query);
+    $result->execute($params);
+    return $result->fetchAll();
+  }
+  public static function querySingle($query, $params = array())
+  {
+    $result = self::queryOne($query, $params);
+    if (!$result)
+      return false;
+    return $result[0];
+  }
+  // Executes a query and returns the number of affected rows
+  public static function query($query, $params = array())
+  {
+    $result = self::$connection->prepare($query);
+    $result->execute($params);
+    return $result->rowCount();
   }
 }
